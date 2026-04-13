39cbc8bed27fc3c65556b100c13b9e459cb6319c
Super-mehdi <Amgharyelmehdi@gmail.com>
2026-04-13 15:42:53 +0100

    Solve one away

--- Arrays and Strings/Main.java
+++ Arrays and Strings/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println(PalindromPermutation.isPermutationOfPalindrome("hello"));
+        System.out.println(OneAway.isOneAway("pale", "pable"));
     }
 }
--- Arrays and Strings/OneAway.java
+++ Arrays and Strings/OneAway.java
@@ -6,17 +6,38 @@ public class OneAway {
     // if len(str1) = len(str2) -1 => check for insertion or deletion.
     // else reject
 
+
+    //My own solution, ugly but it works.
     public static boolean isOneAway(String str1, String str2){
         if (str1.length() > str2.length()){
             String tmp=str1;
             str1=str2;
             str2=tmp;
         }
-        if (str1.length() < str2.length()-1) return false;
-        if (str1.length() == str2.length()){
-            int n=str1.length();
+        if (str1.length() < str2.length()-1) return false;//If str1.length() < str2.length(), the only possible transformation (from str1 t str2) is insertion, which is deletion 
+        int n=str1.length();
+        if (str1.length() == str2.length()){//Check for replacement
+            boolean replacedOnce=false;
             for (int i=0;i<n;i++){
-                if (str1[i] != str2[i]) return false;
+                if (str1.charAt(i) != str2.charAt(i)){ 
+                    if (replacedOnce) return false;
+                    replacedOnce=true;
+                }
+            }
+        }
+        int i=0,j=0;
+        boolean skipped=false;
+        if (str1.length() == str2.length()-1){
+            while (i<n){
+                if (str1.charAt(i)==str2.charAt(j)){
+                    i++;
+                    j++;
+                } else if (!skipped){
+                    j++;
+                    skipped=true;
+                } else {
+                    return false;
+                }
             }
         }
         return true;
