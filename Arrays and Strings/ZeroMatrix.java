304faab128373be335f97370687493dc7e39e954
Super-mehdi <Amgharyelmehdi@gmail.com>
2026-04-20 19:15:28 +0100

    solve zero matrix book way

--- Arrays and Strings/ZeroMatrix.java
+++ Arrays and Strings/ZeroMatrix.java
@@ -25,4 +25,48 @@ public class ZeroMatrix{
             }
         }
     }
+    public static void zeroMatrix2(int[][] matrix){
+        int n=matrix.length,m=matrix[0].length;
+
+        boolean colHasZero=false;
+        boolean rowHasZero=false;
+        for (int i=0;i<n;i++){
+            if (matrix[i][0]==0){
+                 colHasZero=true;
+                 break;
+            }
+        }
+        for (int j=0;j<m;j++){
+            if (matrix[0][j]==0){
+                rowHasZero=true;
+                break;
+            } 
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (matrix[i][j] == 0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for (int i=1;i<n;i++){
+            if (matrix[i][0]==0){
+                for (int j=0;j<m;j++) matrix[i][j]=0;
+            }
+        }
+        for (int j=1;j<m;j++){
+            if (matrix[0][j]==0){
+                for (int i=0;i<n;i++) matrix[i][j]=0;
+            }
+        }
+        if (rowHasZero){
+            for (int j=0;j<m;j++) matrix[0][j]=0;
+        }
+
+        if (colHasZero){
+            for (int i=0;i<n;i++) matrix[i][0]=0;
+        }
+
+    }
 }
