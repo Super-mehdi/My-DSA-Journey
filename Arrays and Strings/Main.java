27d7acf43226f6202c7da5c1fcc9818fbfac6755
Super-mehdi <Amgharyelmehdi@gmail.com>
2026-04-20 14:18:30 +0100

    implement my own solution

--- Arrays and Strings/Main.java
+++ Arrays and Strings/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix = {{1, 2, 3}, {4, 0, 6}, {7, 8, 9}};
         System.out.println(java.util.Arrays.deepToString(matrix));
-        RotateMatrix.rotateMatrix2(matrix);
+        ZeroMatrix.zeroMatrix(matrix);
         System.out.println(java.util.Arrays.deepToString(matrix));
     }
 }
