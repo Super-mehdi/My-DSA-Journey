cd386bc8a0cdb9086a98626606db1935c86d24fe
Super-mehdi <Amgharyelmehdi@gmail.com>
2026-05-27 01:40:40 +0100

    implement follow up on sum lists problem

--- Linked lists/Main.java
+++ Linked lists/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         /*
-            (7-> 1 -> 6) + (5 -> 9 -> 2).That is,617 + 295 = 912
-            Output: 2 -> 1 -> 9. That is, 912.
+        Input:(6 -> 1 -> 7) + (2 -> 9 -> 5).That is,617 + 295.
+        Output: 9 -> 1 -> 2. That is, 912.
         */
-       Node head1=new Node(7),head2=new Node(5);
+       Node head1=new Node(6),head2=new Node(2);
        head1.appendToTail(1);
        head2.appendToTail(9);
-       head1.appendToTail(6);
-       head2.appendToTail(2);
+       head1.appendToTail(7);
+       head2.appendToTail(5);
        head1.printList();
        head2.printList();
-       Node head3=SumLists.sumLists3(head1, head2);
+       Node head3=SumLists.sumLists5(head1, head2);
        head3.printList();
     }
 }
--- Linked lists/SumLists.java
+++ Linked lists/SumLists.java
@@ -12,13 +12,96 @@ public class SumLists {
         lnput:(6 -> 1 -> 7) + (2 -> 9 -> 5).That is,617 + 295.
         Output: 9 -> 1 -> 2. That is, 912.
     */
-    /*
-    The first thing to do is to determine the algorithm :
-    - collect the numbers
-    - sum
-    - divisions & new linked list
-    */
-   
+
+    private static class PartialSum {
+        Node sum=null;
+        int carry=0;
+    }
+    private static Node insertBefore(Node head,int n){
+        Node node=new Node(n);
+        node.next=head;
+        return node;
+    }
+    private static Node paddList(Node head, int n){
+        Node newHead=head;
+        while (n>0){
+            newHead=insertBefore(newHead, 0);
+            n--;
+        }
+        return newHead;
+    }
+    private static int len(Node head){
+        Node currNode=head;
+        int len=0;
+        while (currNode != null){
+            len++;
+            currNode=currNode.next;
+        }
+        return len;
+    }
+
+    public static PartialSum sumListsHelper(Node head1, Node head2){
+        PartialSum result=null;
+        if (head1 == null && head2 == null){
+            result=new PartialSum();
+            return result;
+        }
+        result=sumListsHelper(head1.next, head2.next);
+
+        int val = result.carry + head1.data + head2.data;
+
+        Node fullResult=insertBefore(result.sum, val%10);
+
+        result.sum = fullResult;
+        result.carry = val/10;
+
+        return result;
+    }
+
+    public static Node sumLists5(Node head1, Node head2){
+        Node result=null;
+        int n1=len(head1),n2=len(head2);
+        if (n1 > n2){
+            head2=paddList(head2, n1-n2);
+        } else if (n2 > n1){
+            head1=paddList(head1, n2-n1);
+        }
+        PartialSum sum=sumListsHelper(head1, head2);
+
+        if (sum.carry != 0){
+            result=insertBefore(sum.sum, sum.carry);
+        } else {
+            result=sum.sum;
+        }
+
+        return result;
+    }
+
+
+    public static Node sumLists4(Node head1, Node head2){
+        Node newHead=null;
+        int n=0;
+        Node currNode1=head1,currNode2=head2;
+        while (currNode1!=null || currNode2!=null){
+            int n1=(currNode1!=null)?currNode1.data:0;
+            int n2=(currNode2!=null)?currNode2.data:0;
+            if (newHead == null){
+                newHead = new Node((n1+n2+n)%10);
+            } else {
+                newHead.appendToTail((n1+n2+n)%10);
+            }
+            n=(n1+n2+n)/10;
+            currNode1=(currNode1!=null)?currNode1.next:null;
+            currNode2=(currNode2!=null)?currNode2.next:null;
+        }
+        if (n!=0){
+            newHead.appendToTail(n);
+        }
+        return newHead;
+    }
+
+
+
 
     public static Node sumLists3(Node head1, Node head2){
         Node newHead=null;
